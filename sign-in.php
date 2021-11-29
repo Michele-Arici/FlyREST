83bbc04bc2dfc0a6741f88fd7e4254cbba0f7d2e
Michele-Arici <miki.arici8@gmail.com>
2021-11-29 13:48:34 +0100

    Aggiunto 3 file 29/11/2021

--- sign-in.php
+++ sign-in.php
@@ -1,3 +1,7 @@
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -15,12 +19,12 @@
             <div class="text-center mb-4">
                 <a href="."><img src="./static/logo.svg" height="36" alt=""></a>
             </div>
-            <form class="card card-md" action="." method="get" autocomplete="off">
+            <form class="card card-md" method="POST" autocomplete="off">
                 <div class="card-body">
                     <h2 class="card-title text-center mb-4">Login to your account</h2>
                     <div class="mb-3">
                         <label class="form-label">Username</label>
-                        <input id="username_input" type="text" class="form-control" placeholder="Enter email">
+                        <input name="username" type="text" class="form-control" placeholder="Enter username">
                     </div>
                     <div class="mb-2">
                         <label class="form-label">
@@ -30,22 +34,7 @@
                             </span>
                         </label>
                         <div class="input-group input-group-flat">
-                            <input id="password_input" type="password" class="form-control" placeholder="Password" autocomplete="off">
-                            <span class="input-group-text">
-                                <a href="#" class="link-secondary" title="" data-bs-toggle="tooltip"
-                                    data-bs-original-title="Show password">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <circle cx="12" cy="12" r="2"></circle>
-                                        <path
-                                            d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7">
-                                        </path>
-                                    </svg>
-                                </a>
-                            </span>
+                            <input name="password" type="password" class="form-control" placeholder="Password" autocomplete="off">
                         </div>
                     </div>
                     <div class="mb-2">
@@ -55,7 +44,7 @@
                         </label>
                     </div>
                     <div class="form-footer">
-                        <button id="button-signin" onclick="SignIn()" type="submit" class="btn btn-primary w-100">Sign in</button>
+                        <button type="submit" class="btn btn-primary w-100">Sign in</button>
                     </div>
                 </div>
             </form>
@@ -78,12 +67,28 @@
             tracesSampleRate: 1.0,
         });
     </script>
-    <script>
-        function SignIn() {
+    <?php
+        $username = $password = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $username = test_input($_POST["username"]);
+          $password = test_input($_POST["password"]);
             
+            if (isset($username) && isset($password)) {
+                if ($username == "admin" && $password == "admin") {
+                    header('Location: '. 'admin.php');
+                    $_SESSION['username']='admin';
+                }
+            }
         }
-        document.getElementById('button-signin').onclick 
-    </script>
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>
     <!-- Libs JS -->
     <!-- Tabler Core -->
     <script src="./dist/js/tabler.min.js?1637753930"></script>
