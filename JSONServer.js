7c4812b55cd5115c96f8563c0ed250ba2d2ef9b5
YehorCherednychenko <20003000m76@gmail.com>
2021-12-09 22:40:19 +0100

    JSONServer get method implemented

--- JSONServer.js
+++ JSONServer.js
@@ -4,30 +4,32 @@ var fs = require("fs");
 
 
 
-var Flight = {
-   "Fly4" : {
-      "id" : "maesh",
-      "company" : "password1",
-      "date" : "teacher",
-      "from": 1,
-      "to": 2,
-      "price": 2,
-      "departT": 2,
-      "arrivalT": 2
-   }
-}
+
 
 app.get('/listFlights', function (req, res) {
     fs.readFile( __dirname + "/" + "Flights.json", 'utf8', function (err, data) {
        console.log( data );
-       res.end( data );
+       res.send( data );
     });
  })
 
- app.post('/addFlight', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
-})
+
+
+ app.get('/getFlight/:flight_from/:flight_to/:flight_departure_date', (req, res) => {
+   let responseArray = [];
+   const config = require("./Flights.json");
+   for (const flight in config)
+   {
+      if(config[flight]["flight_from"] === req.params.flight_from && config[flight]["flight_to"] === req.params.flight_to && config[flight]["flight_departure_date"] === req.params.flight_departure_date)
+         responseArray.push(config[flight]);
+   }
+   var jsonObjResp = JSON.stringify(responseArray);
+   res.send(jsonObjResp);
+
+ }) 
+
+ 
+
 
 var server = app.listen(8090, function () {
     var host = server.address().address;
