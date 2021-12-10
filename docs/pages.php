4f64cd11f68e57efc466f4732ec31525d6d965ed
Michele Arici <miki.arici8@gmail.com>
2021-12-10 22:53:23 +0100

    pagine per la documentazione in html

---
+++ docs/pages.php
@@ -0,0 +1,100 @@
+
+
+<head>
+  <meta charset="utf-8">
+  <title>FlyREST</title>
+
+  <script src="https://unpkg.com/@tabler/core@1.0.0-beta4/dist/js/tabler.min.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/@tabler/core@1.0.0-beta4/dist/css/tabler.min.css">
+
+</head>
+
+<body>
+
+<div class="page-wrapper">
+        <div class="container-xl">
+          <!-- Page title -->
+          <div class="page-header d-print-none">
+            <div class="row align-items-center">
+              <div class="col">
+                <h2 class="page-title">
+                  Documentation
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="page-body">
+          <div class="container-xl">
+            <div class="row gx-lg-4">
+              <div class="d-none d-lg-block col-lg-3">
+                <ul class="nav nav-pills nav-vertical">
+                  <li class="nav-item">
+                    <a href="../docs/index.php" class="nav-link">
+                      Introduction
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../docs/getting_started.php" class="nav-link" aria-expanded="false">
+                      Getting started
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../docs/pages.php" class="nav-link active" aria-expanded="false">
+                      Pages
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../docs/methods.php" class="nav-link" aria-expanded="false">
+                      Methods
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="../docs/pages.php" class="nav-link" aria-expanded="false">
+                      ??
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-9">
+                <div class="card card-lg">
+                    <div class="card-body">
+                        <div class="markdown">
+                        <div>
+                            <div class="d-flex mb-3">
+                            <h1 class="m-0">Getting started</h1>
+                            </div>
+                        </div>
+                        <h2 id="what-are-the-benefits">What are the benefits?</h2>
+                        <p>Tabler is a perfect solution if you want to create an interface which is not only user-friendly but also fully responsive. Thanks to the big choice of ready-made components, you can customize your design and adapt it to the needs of even the most demanding users. On top of that, Tabler is an open source solution, continuously developed and improved by the open source community.</p>
+                        <h2 id="set-up-the-environment">Installare l'ambiente</h2>
+                        <p>Requisiti per avviare il progetto:</p>
+                        <ul>
+                            <li>Xampp</li>
+                            <li>Visual Studio Code</li>
+                        </ul>
+                        <h3 id="windows-users">Windows users</h3>
+                        <ol>
+                            <li><a href="https://git-scm.com/download/win">Install Git</a> in <code class="language-plaintext highlighter-rouge">C:\Program Files\git\bin</code> directory and run <code class="language-plaintext highlighter-rouge">npm config set script-shell "C:\Program Files\git\bin\bash.exe"</code> to change the default shell.</li>
+                            <li><a href="https://rubyinstaller.org/downloads/">Install Ruby+Devkit</a> - recommended version is <a href="https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-2.5.5-1/rubyinstaller-devkit-2.5.5-1-x86.exe">2.5.5</a>.</li>
+                            <li><a href="https://jekyllrb.com/docs/installation/windows/">Read guide</a> to get Jekyll up and running without problems.</li>
+                        </ol>
+                        <p>Once you’ve completed the setup, you’ll be able to run the various commands provided from the command line.</p>
+                        <h2 id="build-tabler-locally">Build Tabler locally</h2>
+                        <ol>
+                            <li>From the root <code class="language-plaintext highlighter-rouge">/tabler</code> directory, run <code class="language-plaintext highlighter-rouge">npm run start</code> in the command line.</li>
+                            <li>Open <a href="http://localhost:3000">http://localhost:3000</a> in your browser, and voilà.</li>
+                            <li>Any change in <code class="language-plaintext highlighter-rouge">/src</code> directory will build the application and refresh the page.</li>
+                        </ol>
+                        <h2 id="bugs-and-feature-requests">Bugs and feature requests</h2>
+                        <p>Found a bug or have a feature request? <a href="https://github.com/tabler/tabler/issues/new">Please open a new issue</a>.</p>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
