30d2c73fde1d9ac507fde130d709065f71a923a5
Michele Arici <miki.arici8@gmail.com>
2021-12-10 10:38:42 +0100

    Problems fixed server methods

--- JSONServer.js
+++ JSONServer.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-app.use(express.json);
+app.use(express.json());
 var fs = require("fs");
 const { stringify } = require('querystring');
-
 
 
  app.get('/getFlight/:flight_from/:flight_to/:flight_departure_date', (req, res) => {
    let responseArray = [];
    const config = require("./Flights.json");
+   console.log("yes");
    for (const flight in config)
    {
       if(config[flight]["flight_from"] === req.params.flight_from && config[flight]["flight_to"] === req.params.flight_to && config[flight]["flight_departure_date"] === req.params.flight_departure_date)
@@ -28,9 +28,10 @@ const { stringify } = require('querystring');
       res.status(400).send("Null body");
    }
    
-   const config = require("./Flights.json");
-   const addFlightKey = config.length + 2;
-   config.addFlightKey = req.body;
+   let config = require("./Flights.json");
+   const addFlightKey = Object.keys(config).length + 1;
+   console.log(addFlightKey);
+   config[addFlightKey] = req.body;
    config = JSON.stringify(config);
    fs.writeFile("./Flights.json", config, 'utf8', function (err) {
       if (err) {
@@ -49,13 +50,13 @@ const { stringify } = require('querystring');
  });
 
  app.put('/updateFlight/:id', (req, res) => {
-   const config = require("./Flights.json");
+   let config = require("./Flights.json");
    const idUpdate = req.params.id;
-   if (!myJson.hasOwnProperty(idUpdate))
+   if (!config.hasOwnProperty(idUpdate))
       res.status(404).send("The flight with following id does not exist!");
    else
    {
-      config.idUpdate = req.body;
+      config[idUpdate] = req.body;
       config = JSON.stringify(config);
       fs.writeFile("./Flights.json", config, 'utf8', function (err) {
          if (err) {
@@ -74,9 +75,9 @@ const { stringify } = require('querystring');
 
 app.delete('/deleteFlight/:id', (req, res) => {
 
-   const config = require("./Flights.json");
+   let config = require("./Flights.json");
    const idUpdate = req.params.id;
-   if (!myJson.hasOwnProperty(idUpdate))
+   if (!config.hasOwnProperty(idUpdate))
       res.status(404).send("The flight with following id does not exist!");
    else
    {
@@ -94,12 +95,11 @@ app.delete('/deleteFlight/:id', (req, res) => {
    }
 
 });
-
-
-
-var server = app.listen(8090, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    
-    console.log("Example app listening at http://%s:%s", host, port);
+server = app.listen(8090,  function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  
+  console.log("Example app listening at http://%s:%s", host, port);
 });
+
+
