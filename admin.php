5525ba7a7b16b5e44fbcbffc303ab91da5ff893f
YehorCherednychenko <20003000m76@gmail.com>
2021-12-10 20:24:59 +0100

    getAllData method implemented in admin.php

--- admin.php
+++ admin.php
@@ -27,6 +27,94 @@
 
 <body>
 <script>
+  var company_logo = {"ryanair": "https://q-xx.bstatic.com/data/airlines_logo/square_96/FR.png", "wizz_air": "https://q-xx.bstatic.com/data/airlines_logo/square_96/W6.png", "ita_airlines": "https://q-xx.bstatic.com/data/airlines_logo/square_96/AZ.png", "easyjet": "https://q-xx.bstatic.com/data/airlines_logo/square_96/U2.png", "air_france": "https://q-xx.bstatic.com/data/airlines_logo/square_96/AF.png", "lufthansa": "https://q-xx.bstatic.com/data/airlines_logo/square_96/LH.png", "swiss": "https://q-xx.bstatic.com/data/airlines_logo/square_96/LX.png", "vueling": "https://q-xx.bstatic.com/data/airlines_logo/square_96/VY.png"};
+  var search_result_number = 0;
+  function formatDate (input) {
+    var datePart = input.match(/\d+/g),
+    year = datePart[0], // get only two digits
+    month = datePart[1], day = datePart[2];
+
+    return day+'/'+month+'/'+year;
+  }
+
+  function PrintFlight(f_from, f_to, f_departure_date, f_departure_time, f_arrival_date, f_arrival_time, f_company, f_stages, f_first, f_second, f_economy) {
+    var departure_time = f_departure_time.split(':');
+    var depart_date_split = f_departure_date.match(/\d+/g);
+    var depart_date = new Date(depart_date_split[0], depart_date_split[1], depart_date_split[2], departure_time[0], departure_time[1]);
+
+    var arrival_time = f_arrival_time.split(':');
+    var arrival_date_split = f_arrival_date.match(/\d+/g);
+    var arrival_date = new Date(arrival_date_split[0], arrival_date_split[1], arrival_date_split[2], arrival_time[0], arrival_time[1]);
+
+    var diff = Math.abs(arrival_date - depart_date);
+    var diff_minutes = Math.floor((diff/1000)/60);
+
+    var hours = Math.floor(diff_minutes / 60);          
+    var minutes = diff_minutes % 60;
+      
+    if (f_stages == "0") {
+      f_stages = "Direct";
+    } else {
+      f_stages = f_stages + " stage";
+    }
+
+    var html = `<div class="col-12 mb-3">
+      <div class="card">
+        <div class="row row-0">
+          <div class="col-3 order-md-last text-center" style="border-left: 1px solid rgba(98,105,118,.16);">
+            <div class="card-body">
+              <div class="h1 m-0">€${f_first} €${f_second} €${f_economy} </div>
+              <div class="text-muted small mb-3">FIRST | SECOND | ECONOMY</div>
+            </div>
+          </div>
+          <div class="col" style="display: flex; align-items:center;">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-2" style="background: url(${company_logo[f_company]}) no-repeat center; background-size: contain;"></div>
+                <div class="col-3 text-center">
+                  <div class="h1 m-0">${f_departure_time}</div>
+                  <div class="text-muted">${formatDate(f_departure_date)}</div>
+                </div>
+                <div class="col-4 text-center">
+                  <div class="h1 m-0">${hours} h ${minutes} min</div>
+                  <hr style="margin-top: 4px; margin-bottom: 4px">
+                  <div class="text-muted">${f_stages}</div>
+                </div>
+                <div class="col-3 text-center">
+                  <div class="h1 m-0">${f_arrival_time}</div>
+                  <div class="text-muted">${formatDate(f_arrival_date)}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+    document.getElementById('flight_list').innerHTML += html;
+    search_result_number++;
+  }
+
+  function PrintFlightRemoveForm(f_id, f_from, f_to, f_departure_date, f_company, f_dep_time) {
+    var content = `<label class="form-selectgroup-item flex-fill">
+      <input type="checkbox" name="form-flights-selected[]" value="${f_id}" class="form-selectgroup-input">
+      <div class="form-selectgroup-label d-flex align-items-center p-3">
+        <div class="me-3">
+          <span class="form-selectgroup-check"></span>
+        </div>
+        <div class="form-selectgroup-label-content d-flex align-items-center">
+          <span class="avatar me-3" style="background-image: url(${company_logo[f_company]})"></span>
+          <div>
+            <div class="font-weight-medium">ID: ${f_id}</div>
+            <div class="text-muted">${f_from} | ${f_to} </div>
+            <div class="text-muted">${f_departure_date} | ${f_dep_time} </div>
+          </div>
+        </div>
+      </div>
+    </label>`
+    document.getElementById('remove_flights_list').innerHTML += content;
+  }
+</script>
+<script>
 
 
 async function deleteMethod() {
@@ -96,7 +184,7 @@ function putMethod() {
     "flight_seats": document.getElementById()
   } 
 
-  url = "http://localhost:8090/updateFlight/"  + //id del volo da cancellare
+  url = "http://localhost:8090/updateFlight/"  + "";//id del volo da cancellare
   fetch(url,{
       method:'PUT',
       headers:{
@@ -104,6 +192,9 @@ function putMethod() {
       },
       body:JSON.stringify(jsonDataToSend)
   });
+
+
+  
 }
 
 </script>
@@ -322,125 +413,41 @@ function putMethod() {
     </div>
   </div>
 
-  <script>
-    var company_logo = {"ryanair": "https://q-xx.bstatic.com/data/airlines_logo/square_96/FR.png", "wizz_air": "https://q-xx.bstatic.com/data/airlines_logo/square_96/W6.png", "ita_airlines": "https://q-xx.bstatic.com/data/airlines_logo/square_96/AZ.png", "easyjet": "https://q-xx.bstatic.com/data/airlines_logo/square_96/U2.png", "air_france": "https://q-xx.bstatic.com/data/airlines_logo/square_96/AF.png", "lufthansa": "https://q-xx.bstatic.com/data/airlines_logo/square_96/LH.png", "swiss": "https://q-xx.bstatic.com/data/airlines_logo/square_96/LX.png", "vueling": "https://q-xx.bstatic.com/data/airlines_logo/square_96/VY.png"};
-    var search_result_number = 0;
-    function formatDate (input) {
-      var datePart = input.match(/\d+/g),
-      year = datePart[0], // get only two digits
-      month = datePart[1], day = datePart[2];
+<script>
+  async function getAllData(){
+    let api_url = "http://localhost:8090/getFlight";
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data);
+    for (const flights in data)
+    {
+      let flight_from = data[flights]["flight_from"];
+      let flight_to = data[flights]["flight_to"];
+      let flight_departure_date = data[flights]["flight_departure_date"];
+      let flight_departure_time = data[flights]["flight_departure_time"]; 
+      let flight_arrival_date = data[flights]["flight_arrival_date"];
+      let flight_arrival_time = data[flights]["flight_arrival_time"]; 
+      let flight_first_class = data[flights][ "flight_first_class"];
+      let flight_second_class = data[flights]["flight_second_class"];
+      let flight_economy_class = data[flights]["flight_economy_class"];
+      let flight_stages = data[flights]["flight_stages"];
+      let flight_company = data[flights]["flight_company"];
+      let flight_seats = data[flights]["flight_seats"];
 
-      return day+'/'+month+'/'+year;
+
+      PrintFlight(flight_from, flight_to,flight_departure_date,flight_departure_time,flight_arrival_date,flight_arrival_time,flight_company,flight_stages,flight_first_class, flight_second_class, flight_economy_class)
+      PrintFlightRemoveForm(flights,flight_from, flight_to,flight_departure_date,flight_company,flight_departure_time);
     }
 
-    function PrintFlight(f_from, f_to, f_departure_date, f_departure_time, f_arrival_date, f_arrival_time, f_company, f_stages, f_first, f_second, f_economy) {
-      var departure_time = f_departure_time.split(':');
-      var depart_date_split = f_departure_date.match(/\d+/g);
-      var depart_date = new Date(depart_date_split[0], depart_date_split[1], depart_date_split[2], departure_time[0], departure_time[1]);
-
-      var arrival_time = f_arrival_time.split(':');
-      var arrival_date_split = f_arrival_date.match(/\d+/g);
-      var arrival_date = new Date(arrival_date_split[0], arrival_date_split[1], arrival_date_split[2], arrival_time[0], arrival_time[1]);
-
-      var diff = Math.abs(arrival_date - depart_date);
-      var diff_minutes = Math.floor((diff/1000)/60);
-
-      var hours = Math.floor(diff_minutes / 60);          
-      var minutes = diff_minutes % 60;
-      
-      if (f_stages == "0") {
-        f_stages = "Direct";
-      } else {
-        f_stages = f_stages + " stage";
-      }
-
-      var html = `<div class="col-12 mb-3">
-        <div class="card">
-          <div class="row row-0">
-            <div class="col-3 order-md-last text-center" style="border-left: 1px solid rgba(98,105,118,.16);">
-              <div class="card-body">
-                <div class="h1 m-0">€${f_first} €${f_second} €${f_economy} </div>
-                <div class="text-muted small mb-3">FIRST | SECOND | ECONOMY</div>
-              </div>
-            </div>
-            <div class="col" style="display: flex; align-items:center;">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-2" style="background: url(${company_logo[f_company]}) no-repeat center; background-size: contain;"></div>
-                  <div class="col-3 text-center">
-                    <div class="h1 m-0">${f_departure_time}</div>
-                    <div class="text-muted">${formatDate(f_departure_date)}</div>
-                  </div>
-                  <div class="col-4 text-center">
-                    <div class="h1 m-0">${hours} h ${minutes} min</div>
-                    <hr style="margin-top: 4px; margin-bottom: 4px">
-                    <div class="text-muted">${f_stages}</div>
-                  </div>
-                  <div class="col-3 text-center">
-                    <div class="h1 m-0">${f_arrival_time}</div>
-                    <div class="text-muted">${formatDate(f_arrival_date)}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>`
-      document.getElementById('flight_list').innerHTML += html;
-      search_result_number++;
-    }
-
-    function PrintFlightRemoveForm(f_id, f_from, f_to, f_departure_date, f_company, f_dep_time) {
-      var content = `<label class="form-selectgroup-item flex-fill">
-        <input type="checkbox" name="form-flights-selected[]" value="${f_id}" class="form-selectgroup-input">
-        <div class="form-selectgroup-label d-flex align-items-center p-3">
-          <div class="me-3">
-            <span class="form-selectgroup-check"></span>
-          </div>
-          <div class="form-selectgroup-label-content d-flex align-items-center">
-            <span class="avatar me-3" style="background-image: url(${company_logo[f_company]})"></span>
-            <div>
-              <div class="font-weight-medium">ID: ${f_id}</div>
-              <div class="text-muted">${f_from} | ${f_to} </div>
-              <div class="text-muted">${f_departure_date} | ${f_dep_time} </div>
-            </div>
-          </div>
-        </div>
-      </label>`
-      document.getElementById('remove_flights_list').innerHTML += content;
-    }
-  </script>
+  }
+</script>
   <?php
     $endtime = microtime(true);
     $loading_page_time = $endtime - $starttime;
 
-    $string = file_get_contents("Flights.json");
-    if ($string === false) {
-        // deal with error...
-    }
-
-    $json_a = json_decode($string, true);
-    if ($json_a === null) {
-        // deal with error...
-    }
-
-    foreach ($json_a as $flight_n => $flight_a) {
-      $f_id = $flight_n;
-      $f_from = $flight_a["flight_from"];
-      $f_to = $flight_a["flight_to"];
-      $f_departure_date = $flight_a["flight_departure_date"];
-      $f_departure_time = $flight_a["flight_departure_time"];
-      $f_arrival_date = $flight_a["flight_arrival_date"];
-      $f_arrival_time = $flight_a["flight_arrival_time"];
-      $f_first_class = $flight_a["flight_first_class"];
-      $f_second_class = $flight_a["flight_second_class"];
-      $f_economy_class = $flight_a["flight_economy_class"];
-      $f_company = $flight_a["flight_company"];
-      $f_stages = $flight_a["flight_stages"];
-
-      echo "<script>PrintFlight('".$f_from."','".$f_to."','".$f_departure_date."','".$f_departure_time."','".$f_arrival_date."','".$f_arrival_time."','".$f_company."','".$f_stages."','".$f_first_class."','".$f_second_class."','".$f_economy_class."')</script>";
-      echo "<script>PrintFlightRemoveForm('".$f_id."','".$f_from."','".$f_to."','".$f_departure_date."','".$f_company."','".$f_departure_time."')</script>";
-    }
+    
+    echo "<script>getAllData();</script>";
+    
     
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
