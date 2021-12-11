5aa29a2e3bcc67c8d247ceee385b3e6094917994
FrancescoTonassi <57535330+FrancescoTonassi@users.noreply.github.com>
2021-12-11 19:04:30 +0100

    Added table

--- docs/methods.php
+++ docs/methods.php
@@ -55,38 +55,92 @@
                 <div class="card card-lg">
                     <div class="card-body">
                         <div class="markdown">
-                        <div>
-                            <div class="d-flex mb-3">
-                            <h1 class="m-0">Metodi implementati</h1>
-                            </div>
+                          <div>
+                              <div class="d-flex mb-3">
+                              <h1 class="m-0">Metodi implementati</h1>
+                              </div>
+                          </div>
+                          <h2 id="what-are-the-benefits">POST</h2>
+                          <p>Il metodo POST viene utilizzato per creare un nuovo volo da parte di un Amministratore</p>
+                          <img src="..\img\POST.png">
+                          </br>
+                          <p>Il metodo POST prende come parametri tutti i dati relativi al volo da creare</p>
+                          <h2 id="what-are-the-benefits">GET</h2>
+                          <p>Il metodo GET viene utilizzato per effettuare la ricerca di un volo che deve essere già esistente nel database (file Flights.json), la ricerca viene esegueta dall'utente</p>
+                          <img src="..\img\GET.png">
+                          </br>
+                          <p>Il metodo GET prende come parametri :</p>
+                          <ul>
+                            <li>Data di partenza</li>
+                            <li>Data di arrivo</li>
+                            <li>Luogo di partenza</li>
+                            <li>Totale persone</li>
+                            <li>Scelta della classe</li>
+                          </ul>
+                          <h2 id="what-are-the-benefits">DELETE</h2>
+                          <p>Il metodo DELETE viene utilizzato per rimuovere un volo tra quelli esistenti da parte di un Amministratore</p>
+                          <img src="..\img\DELETE.png">
+                          </br>
+                          <p>Il metodo DELETE prende come parametro l'id del volo da rimuovere, per ripristinare gli id dei voli una volta rimossi è stato creato un metodo "deleteUpdateIdMethod"</p>
+                          <h2 id="what-are-the-benefits">PUT</h2>
+                          <p>Il metodo PUT viene utilizzato per aggiornare un volo già esistente da parte di un Amministratore</p>
+                          <img src="..\img\">
+                          </br>
+                          <p>Il metodo PUT prende come parametri ...</p>
+
+                          <br>
+                          <h1 id="what-are-the-benefits">Tabella riassuntiva dei metodi</h1>
+                          <div class="table-responsive">
+                            <table
+                              class="table table-vcenter">
+                              <thead>
+                                <tr>
+                                  <th>Name</th>
+                                  <th>URL</th>
+                                  <th>Role</th>
+                                  <th class="w-1"></th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td >POST</td>
+                                  <td class="text-muted" >
+                                    http://localhost:8090/addFlight
+                                  </td>                         
+                                  <td class="text-muted" >
+                                    Admin
+                                  </td>                               
+                                </tr>
+                                <tr>
+                                  <td >GET</td>
+                                  <td class="text-muted" >
+                                    http://localhost:8090/getFlight/
+                                  </td>
+                                  <td class="text-muted" >
+                                    User
+                                  </td>                            
+                                </tr>
+                                <tr>
+                                  <td >PUT</td>
+                                  <td class="text-muted" >
+                                    http://localhost:8090/updateFlight/
+                                  </td>
+                                  <td class="text-muted" >
+                                    Admin
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <td >DELETE</td>
+                                  <td class="text-muted" >
+                                    http://localhost:8090/deleteUpdateFlight
+                                  </td>
+                                  <td class="text-muted" >
+                                    Admin
+                                  </td>
+                                </tr>                     
+                              </tbody>
+                            </table>
                         </div>
-                        <h2 id="what-are-the-benefits">POST</h2>
-                        <p>Il metodo POST viene utilizzato per creare un nuovo volo da parte di un Amministratore</p>
-                        <img src="..\img\POST.png">
-                        </br>
-                        <p>Il metodo POST prende come parametri tutti i dati relativi al volo da creare</p>
-                        <h2 id="what-are-the-benefits">GET</h2>
-                        <p>Il metodo GET viene utilizzato per effettuare la ricerca di un volo che deve essere già esistente nel database (file Flights.json), la ricerca viene esegueta dall'utente</p>
-                        <img src="..\img\GET.png">
-                        </br>
-                        <p>Il metodo GET prende come parametri :</p>
-                        <ul>
-                          <li>Data di partenza</li>
-                          <li>Data di arrivo</li>
-                          <li>Luogo di partenza</li>
-                          <li>Totale persone</li>
-                          <li>Scelta della classe</li>
-                        </ul>
-                        <h2 id="what-are-the-benefits">DELETE</h2>
-                        <p>Il metodo DELETE viene utilizzato per rimuovere un volo tra quelli esistenti da parte di un Amministratore</p>
-                        <img src="..\img\DELETE.png">
-                        </br>
-                        <p>Il metodo DELE prende come parametro l'id del volo da rimuovere, per ripristinare gli id dei voli una volta rimossi è stato creato un metodo "deleteUpdateIdMethod"</p>
-                        <h2 id="what-are-the-benefits">PUT</h2>
-                        <p>Il metodo PUT viene utilizzato per aggiornare un volo già esistente da parte di un Amministratore</p>
-                        <img src="..\img\">
-                        </br>
-                        <p>Il metodo PUT prende come parametri ...</p>
                     </div>
                 </div>
               </div>
